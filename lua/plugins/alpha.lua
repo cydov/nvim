17f5aae7bbe7331bc83af24e0a7764a0dda88315
Cyd Bastian <cydov@tuta.io>
2024-01-14 09:50:53 -0700

    Changed the Dashboard.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,18 +3,29 @@ return {
    dependencies = { 'nvim-tree/nvim-web-devicons' },
    config = function()
       local alpha = require('alpha')
-      local dashboard = require('alpha.themes.startify')
+      local dashboard = require('alpha.themes.dashboard')
 
-      dashboard.section.header.val = {
-         [[                                                                       ]],
-         [[       ███████████           █████      ██                       ]],
-         [[      ███████████             █████                               ]],
-         [[      ████████████████ ███████████ ███   ███████       ]],
-         [[     ████████████████ ████████████ █████ ██████████████     ]],
-         [[    ██████████████    █████████████ █████ █████ ████ █████     ]],
-         [[  ██████████████████████████████████ █████ █████ ████ █████    ]],
-         [[ ██████  ███ █████████████████ ████ █████ █████ ████ ██████   ]],
+      local logo = [[
+                                                       
+                ███████████           █████      ██
+               ███████████             █████ 
+               ████████████████ ███████████ ███   ███████
+              ████████████████ ████████████ █████ ██████████████
+             ██████████████    █████████████ █████ █████ ████ █████
+           ██████████████████████████████████ █████ █████ ████ █████
+          ██████  ███ █████████████████ ████ █████ █████ ████ ██████
+      ]]
+      dashboard.section.header.val = vim.split(logo, '\n')
+      dashboard.section.buttons.val = {
+         dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+         dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+         dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+         dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.opts.layout[1].val = 6
 
       alpha.setup(dashboard.opts)
    end
